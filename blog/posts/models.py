eee07bf18591043225f0117391e05695c71908b9
Luis Renan Pavanello <102007776+luisrpavanelli@users.noreply.github.com>
2024-01-25 18:27:35 -0300

    Modify

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,5 +5,5 @@ from datetime import datetime
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=10000)        
-    name = models.CharField(max_length=50)                 
+    name = models.CharField(max_length=50, blank=True, null=True)                 
     created_at = models.DateTimeField(default=datetime.now, blank=True)
